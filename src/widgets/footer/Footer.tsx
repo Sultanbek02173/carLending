afde3d745ff8aa27262c60f4cad62b82e127d213
Sultanbek02173 <sultan.02173@gmail.com>
2025-03-18 16:22:38 +0600

    translate footer

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -1,14 +1,16 @@
 import { FaTelegram, FaTiktok, FaWhatsapp } from 'react-icons/fa';
 import './footer.scss';
 import { CiInstagram } from 'react-icons/ci';
+import { useTranslation } from 'react-i18next';
 
 export const Footer = () => {
+  const { t } = useTranslation();
   return (
     <footer className="main__footer">
       <div className='main__footer__links row'>
         <div className='main__footer__social'>
           <h3>Transfer <sup>24</sup></h3>
-          <p>Your best partner for transportation services</p>
+          <p>{t('footer.description')}</p>
 
           <div className='social row'>
             <FaTelegram />
@@ -20,34 +22,34 @@ export const Footer = () => {
 
         <div className='main__footer__link row'>
           <div>
-            <h3>Home</h3>
-            <p>About Us</p>
-            <p>Services</p>
-            <p>Tariffs</p>
-            <p>Reviews</p>
+            <h3>{t('footer.Home')}</h3>
+            <p>{t('header.about')}</p>
+            <p>{t('header.Services')}</p>
+            <p>{t('header.Tariffs')}</p>
+            <p>{t('header.Reviews')}</p>
           </div>
           <div>
-            <h3>About Us</h3>
-            <p>Support</p>
-            <p>Drivers</p>
+            <h3>{t('header.about')}</h3>
+            <p>{t('footer.Support')}</p>
+            <p>{t('footer.Drivers')}</p>
           </div>
           <div>
-            <h3>Services</h3>
-            <p>Support</p>
-            <p>Business Meetings</p>
-            <p>Events</p>
+            <h3>{t('header.Services')}</h3>
+            <p>{t('footer.Support')}</p>
+            <p>{t('footer.Business')}</p>
+            <p>{t('footer.Events')}</p>
           </div>
           <div>
-            <h3>Tariffs</h3>
+            <h3>{t('header.Tariffs')}</h3>
             <p>Economy</p>
             <p>Comfort</p>
             <p>Premium</p>
             <p>Premium+</p>
           </div>
           <div>
-            <h3>Reviews</h3>
-            <p>Video Reviews</p>
-            <p>Text Reviews</p>
+            <h3>{t('header.Reviews')}</h3>
+            <p>{t('footer.Video')}</p>
+            <p>{t('footer.Text')}</p>
           </div>
         </div>
       </div>
